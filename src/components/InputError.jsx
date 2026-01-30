fad9133df3ae74cefeb5ee8c02cfe38fa36880af
Pc Silva <falecompcsilvadev@gmail.com>
2026-01-30 12:15:58 -0300

    chore: add propTypes validation to InputError component

--- src/components/InputError.jsx
+++ src/components/InputError.jsx
@@ -1,5 +1,11 @@
+import PropTypes from "prop-types";
+
 export const InputError = ({ children }) => {
   return (
     <span className="text-brand-danger text-left text-xs">{children}</span>
   );
+};
+
+InputError.propTypes = {
+  children: PropTypes.node.isRequired,
 };
